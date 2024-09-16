b81c880f97d3f6ce2e0692864f5d185948f040f6
Filip Stojanovic <philsty92@gmail.com>
2024-09-16 18:10:35 -0400

    Added metrics to head.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				<script
+					defer
+					src='https://shown.io/metrics/Lj3bZLpa3l'
+					type='text/javascript'
+				></script>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
